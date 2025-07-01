aa397ca9f3c27b1df34a095662a46987c36ec64f
Siam Hossain <152617928+hossainsiam133@users.noreply.github.com>
2025-07-01 18:12:08 +0600

    Create Output.py

---
+++ Basics/Output.py
@@ -0,0 +1,10 @@
+print("My Name is Siam Hossain");
+print("My ID is: " + "23100084");
+
+print("My Phone" + "Number is: " + str(+8801909967161))
+
+Name = "Talha Mahmud"
+ID = 23100069
+
+print(f"My friend name is: {Name}")
+print(f"His ID is: {ID}")
