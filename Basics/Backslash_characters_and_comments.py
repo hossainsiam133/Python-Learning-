00b645f1a9363c05fdcfe660462caf6ac6f55437
Siam Hossain <152617928+hossainsiam133@users.noreply.github.com>
2025-07-01 18:24:01 +0600

    Add files via upload

---
+++ Basics/Backslash_characters_and_comments.py
@@ -0,0 +1,13 @@
+# Single Line Comments -> print("My Name is Siam Hossain");
+
+""" Multi Line Comments ->
+print("My ID is: " + "23100084");
+
+print("My Phone" + "Number is: " + str(+8801909967161))
+"""
+Name = "Talha Mahmud"
+ID = 23100069
+
+print(f"My Friend Name is:\n {Name}")
+print(f"His ID is:\t {ID}")
+print("His District is: \"Munshiganj\"")
