002fad441f2be732cc7111788a5edcb2e05e1fcf
Siam Hossain <152617928+hossainsiam133@users.noreply.github.com>
2025-07-02 11:49:33 +0600

    Add files via upload

---
+++ Basics/Type_Casting.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter number 1: "))
+num2 = float(input("Enter number 2: "))
+
+result = num1 + num2
+print("Num1 + num2 =",result)
+
+result = num1 - num2
+print("Num1 - num2 =",result)
