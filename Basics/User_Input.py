39b4ec3a8a7b8c8d10d69a49b48b02bf2b4606dd
Siam Hossain <152617928+hossainsiam133@users.noreply.github.com>
2025-07-02 11:43:28 +0600

    Add files via upload

---
+++ Basics/User_Input.py
@@ -0,0 +1,9 @@
+## name,age,cgpa = None,None,None
+## name, age, cgpa = str, int, float
+name = input("Enter your Name: ")
+age = input("Enter your Age: ")
+cgpa = input("Enter your CGPA: ")
+print(f"--------- \t --------")
+print("Name: " + name)
+print("Age: " + age)
+print(f"CGPA: {cgpa}")
