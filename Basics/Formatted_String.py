cfb78091ebc9d383e198432549db53846d772790
Siam Hossain <152617928+hossainsiam133@users.noreply.github.com>
2025-07-03 11:07:47 +0600

    Add files via upload

---
+++ Basics/Formatted_String.py
@@ -0,0 +1,11 @@
+num1 = 20
+num2 = 30.5
+print(type(num1))
+print(type(num2))
+
+print(f"{num1} + {num2} = {num1+num2}")
+print(num1, "+", num2, "=", num1 + num2)
+print(str(num1) + " + " + str(num2) + " = " + str(num1 + num2) + f"\n")
+
+print("Siam Hossain", end=" ")
+print("01909967161")
