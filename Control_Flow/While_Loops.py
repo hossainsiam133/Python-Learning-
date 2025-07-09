2ce62f0700684de053e9d8882c2a9c36346abb83
Siam Hossain <152617928+hossainsiam133@users.noreply.github.com>
2025-07-09 11:42:41 +0600

    Add files via upload

---
+++ Control_Flow/While_Loops.py
@@ -0,0 +1,12 @@
+i = 0
+while i <= 5:
+    print(i)
+    i += 1
+print(f"\n")
+while i >= 0:
+    print(i)
+    i -= 1
+else:
+    print("Loop ends with i =", i)
+
+# Do-While Loops doesn't exist in python
